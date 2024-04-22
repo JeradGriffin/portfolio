e5c554835aa0e983b8040730dd7e54ace904a4cc
Jerad Griffin <jerad.griffin@gmail.com>
2024-04-22 14:55:42 -0400

    Added fulfillment policy

---
+++ pages/fulfillment.js
@@ -0,0 +1,149 @@
+
+
+function Fulfillment() {
+    return (
+        <div className="fulfillment_container">
+            <article>
+                <h1>Fulfillment Policy</h1>
+
+                <p>
+                    These policies will aid users determine how Jerad Griffin fulfills orders
+                    and payments made by credit card via the payment processor Stripe.
+                </p>
+
+                <p>
+                    The following terminology applies to these Terms and Conditions,
+                    Privacy Statement and Disclaimer Notice and all Agreements:
+                    “Client”, “You” and “Your” refers to you, the person on this website
+                    and compliant to the Company’s terms and conditions, either therein
+                    or in a separate written contract. “The Company”, “Ourselves”, “We”,
+                    “Our” and “Us”, refers to our Company. “Party”, “Parties”, or “Us”,
+                    refers to both the Client and ourselves. All terms refer to the
+                    offer, acceptance and consideration of payment necessary to und
+                    ertake the process of our assistance to the Client in the most
+                    appropriate manner for the express purpose of meeting the Client’s
+                    needs in respect of provision of the Company’s stated services, in
+                    accordance with and subject to, prevailing laws of the State of New
+                    York. Any use of the above terminology or other words in the
+                    singular, plural, capitalization and/or he/she or they, are taken as
+                    interchangeable and therefore as referring to same.
+                </p>
+
+                <h2>Fulfillment Policy</h2>
+
+                <p>
+                    At Jerad Griffin, we ensure that the services we provide are
+                    satisfactory. Customer satisfaction is our utmost priority as we
+                    take all complaints about the services we render very seriously.
+                    This Fulfillment Policy (the “Policy”) informs you about our
+                    delivery policy and the rules and guidelines relating to the refunds
+                    for services purchased from our Company. Please read this Policy
+                    carefully to understand your rights and also understand the
+                    requirements for the refund. Our services are billed in U.S.
+                    Dollars.
+                </p>
+
+                <h3>Delivery Policy</h3>
+
+                <p>
+                    At Jerad Griffin, we sell digital marketing services. We deliver our
+                    services to clients via electronic and telephonic means pursuant to
+                    our client’s requests.
+                </p>
+
+                <h3>Refunds</h3>
+
+                <p>We may offer returns in the following conditions:</p>
+
+                <p>
+                    If the service has not been performed at all during the term of our
+                    contract.
+                </p>
+
+                <h3>Exceptions</h3>
+
+                <p>
+                    Notwithstanding other provisions of this Policy, we may refuse to
+                    grant returns or refunds if:
+                </p>
+
+                <ul>
+                    <li>
+                    You received what you ordered, but simply changed your mind about
+                    the service.
+                    </li>
+                    <li>
+                    You were aware or notified of the problems with the service before
+                    making the purchase.
+                    </li>
+                    <li>
+                    You did not like your purchase or realized you had no use of it.
+                    </li>
+                    <li>
+                    You received the service/product and failed to make a report
+                    within 7 days from the date of the receipt
+                    </li>
+                </ul>
+
+                <h3>Return Policy</h3>
+
+                <p>
+                    At Jerad Griffin, we sell digital marketing services. No returns can be
+                    made but in some circumstances we offer refunds. Our refund policy
+                    has been explained above.
+                </p>
+
+                <h3>Cancellation Policy</h3>
+
+                <p>The following are to note:</p>
+
+                <ul>
+                    <li>
+                    Once a contract with Jerad Griffin has been signed, cancellations are
+                    not permitted
+                    </li>
+                </ul>
+
+                <h3>Project Abandonment</h3>
+
+                <p>
+                    If after repeated attempts by Jerad Griffin to begin service, continue
+                    service, or finalize the delivery of the Services, Client fails to
+                    participate, or becomes otherwise unresponsive to Jerad Griffin’s
+                    requests and such continues for a period of thirty (30) days, the
+                    project will, at the option of Jerad Griffin, be abandoned, whereupon Jerad Griffin may reduce any refund for the Services to the Client to zero,
+                    and Client hereby provides its agreement to such forfeiture of any
+                    amounts of the Fees that have been paid by the Client for the
+                    Services as purchased or as described in the original Work Order,
+                    wherever applicable.
+                </p>
+
+                <h3>Administrative Fee</h3>
+
+                <p>The following are to note:</p>
+
+                <ul>
+                    <li>All refunds are subject to 7% administrative fee charges.</li>
+                </ul>
+
+                <h3>How to make a complaint</h3>
+
+                <p>
+                    If you are dissatisfied with the service provided to you and you are
+                    eligible for a refund under this Policy, you must send an email to
+                    info@dryan.com stating your complaints and we will immediately
+                    verify your complaints. Refunds are issued at our discretion as they
+                    may be accepted or rejected.
+                </p>
+
+                <h3>Contact Us &amp; Customer Service Contact Details</h3>
+
+                <p>
+                    For enquiries or comments regarding this Policy, customers may email <a href="mailto:jerad.griffin@gmail.com">jerad.griffin@gmail.com</a>
+                </p>
+            </article>
+        </div>
+    );
+}
+
+export default Fulfillment;
